9e03261bad68ba10e2fd10ad719bafe6d6da15cd
Claude Mamo <claude.mamo@ricston.com>
2012-08-18 11:33:38 +0200

    First commit

---
+++ src/main/java/org/opensourcesoftwareandme/FileAttachmentActivity.java
@@ -0,0 +1,65 @@
+package org.opensourcesoftwareandme;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.ValueCallback;
+import org.apache.cordova.CordovaChromeClient;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.DroidGap;
+import org.apache.cordova.api.CordovaInterface;
+
+public class FileAttachmentActivity extends DroidGap {
+
+    private ValueCallback<Uri> mUploadMessage;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+
+        this.appView.setWebChromeClient(new FileAttachmentChromeClient(this, this.appView));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage) return;
+            Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+    }
+
+    // openFileChooser is an overridable method in WebChromeClient which wasn't
+    // included in the SDK's Android stub code
+    public class FileAttachmentChromeClient extends CordovaChromeClient {
+
+        public FileAttachmentChromeClient(CordovaInterface ctx, CordovaWebView app) {
+            super(ctx, app);
+
+        }
+
+        // For Android > 3.x
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+
+            FileAttachmentActivity.this.startActivityForResult(Intent.createChooser(i, "Choose type of attachment"), FILECHOOSER_RESULTCODE);
+
+        }
+
+        // For Android < 3.x
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+            FileAttachmentActivity.this.startActivityForResult(Intent.createChooser(i, "Choose type of attachment"), FILECHOOSER_RESULTCODE);
+        }
+
+    }
+}
